fb3e290bd9b454f04fee3cd7ee4d1d8de58a0568
Flavien LAUREAU <flavien.laureau@gmail.com>
2020-12-20 01:10:11 +0100

    action on click on blobs
    the menu goes up and borders displaing on divs infos

--- menu.js
+++ menu.js
@@ -1,12 +1,56 @@
-const containerMenu = document.querySelector('.container-menu');
-const btnMenu = document.querySelector('.btn-menu');
-const iconBars = document.querySelector('#icon-bars');
-const iconSmiley = document.querySelector('#icon-smiley');
+const containerMenuElt = document.querySelector('.container-menu');
+const menuElt = document.querySelector('.menu');
+const blobsElts = document.querySelectorAll('.blob');
+const btnMenuElt = document.querySelector('.btn-menu');
+const iconBarsElt = document.querySelector('#icon-bars');
+const iconSmileyElt = document.querySelector('#icon-smiley');
+
+const containerInfoElt = document.querySelector('.container-info');
+const infosElts = document.querySelectorAll('.info');
 
 
-btnMenu.addEventListener('click', () => {
-    containerMenu.classList.add('active');
-    iconBars.style.opacity = "0";
-    iconSmiley.style.opacity = "1";
+
+btnMenuElt.addEventListener('click', () => {
+    menuElt.classList.add('active');
+    iconBarsElt.style.opacity = "0";
+    iconSmileyElt.style.opacity = "1";
 
 })
+
+let menuUp = false;
+blobsElts.forEach(elt => {
+    elt.addEventListener('click', (e) => {
+        if(!menuUp) {
+            console.log(e.currentTarget);
+            containerMenuElt.style.height = "50vh";
+            containerInfoElt.style.display = "block";
+            menuUp = true;
+        }
+
+        infosElts.forEach(element => {
+            element.style.display = "none";
+        });
+        
+        /* switch (e.currentTarget.classList[1]) {
+            case "blob-1":
+                
+                infosElts[0].style.display = "block";
+                break;
+            case "blob-2":
+                infosElts[1].style.display = "block";
+                break;
+            case "blob-3":
+                break;
+        } */
+
+        for (let i = 1; i <= 8; i++) {
+            if(e.currentTarget.classList[1] === `blob-${i}`) {
+                infosElts[i - 1].style.display = "block";
+            }
+        }
+
+    })
+});
+
+
+
